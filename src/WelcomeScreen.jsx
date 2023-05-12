e5ce3579a73bab24d10eca8a23c9590e246e8051
ElenaUJ <elena-ha@web.de>
2023-05-12 11:07:35 -0400

    Adjusted WelcomeScreen class names

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -3,16 +3,16 @@ import './WelcomeScreen.css';
 
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ? (
-    <div className="WelcomeScreen">
+    <div class="WelcomeScreen">
       <h1>Welcome to the EventScout App</h1>
       <h4>
         Log in to see upcoming events around the world for full-stack developers
       </h4>
-      <div className="button_cont" align="center">
-        <div className="google-btn">
-          <div className="google-icon-wrapper">
+      <div class="button_cont" align="center">
+        <div class="google-btn">
+          <div class="google-icon-wrapper">
             <img
-              className="google-icon"
+              class="google-icon"
               src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
               alt="Google sign-in"
             />
@@ -21,9 +21,8 @@ function WelcomeScreen(props) {
             onClick={() => {
               props.getAccessToken();
             }}
-            // Security attributes
             rel="nofollow noopener"
-            className="btn-text"
+            class="btn-text"
           >
             <b>Sign in with Google</b>
           </button>
