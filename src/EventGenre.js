6400180a0ba5253307eb9daa45188093bf4eeaec
ElenaUJ <elena-ha@web.de>
2023-05-17 10:02:52 -0400

    Changed EventGenre component to be class component (for consistency)

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,36 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+import React, { Component } from 'react';
+import { PieChart, Pie, ResponsiveContainer } from 'recharts';
 
-const EventGenre = ({ events }) => {
-  function getData() {
+class EventGenre extends Component {
+  getData = () => {
     const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
     const data = genres.map((genre) => {
-      const value = events.filter((event) => {
+      const value = this.props.events.filter((event) => {
         return event.summary.split(' ').includes(genre);
       }).length;
       return { name: genre, value };
     });
     return data;
-  }
+  };
 
-  return (
-    <ResponsiveContainer height={400}>
-      <PieChart width={400} height={400}>
-        <Pie
-          data={getData()}
-          cx={200}
-          cy={200}
-          labelLine={false}
-          label={({ name, percent }) =>
-            `${name} ${(percent * 100).toFixed(0)}%`
-          }
-          outerRadius={80}
-          fill="#8884d8"
-          dataKey="value"
-        ></Pie>
-      </PieChart>
-    </ResponsiveContainer>
-  );
-};
+  render() {
+    return (
+      <ResponsiveContainer height={400}>
+        <PieChart width={400} height={400}>
+          <Pie
+            data={this.getData()}
+            cx={200}
+            cy={200}
+            labelLine={false}
+            label={({ name, percent }) =>
+              `${name} ${(percent * 100).toFixed(0)}%`
+            }
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+          ></Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    );
+  }
+}
 
 export { EventGenre };
