6382e4b08d8ca5acb880b7014fc2cfca725e94f6
ElenaUJ <elena-ha@web.de>
2023-05-18 10:57:21 -0400

    Changed EventsGenre component back to functional, integrated useEffect, useState

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,50 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
-class EventGenre extends Component {
-  getData = () => {
+function EventGenre({ events }) {
+  const [data, setData] = useState([]);
+
+  function getData() {
     const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
     const data = genres.map((genre) => {
-      const value = this.props.events.filter((event) => {
+      const value = events.filter((event) => {
         return event.summary.split(' ').includes(genre);
       }).length;
       return { name: genre, value };
     });
     return data;
-  };
-
-  render() {
-    const colors = ['#2c3e50', '#27ae60', '#f39c12', '#808080', '#3498db'];
-
-    return (
-      <>
-        <h4>Popular event genres</h4>
-        <ResponsiveContainer height={400}>
-          <PieChart width={400} height={400}>
-            <Pie
-              data={this.getData()}
-              cx={200}
-              cy={200}
-              labelLine={false}
-              label={({ name, percent }) => {
-                if (percent > 0) {
-                  return `${name} ${(percent * 100).toFixed(0)}%`;
-                } else {
-                  return null;
-                }
-              }}
-              outerRadius={80}
-              dataKey="value"
-            >
-              {this.getData().map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={colors[index]} />
-              ))}
-            </Pie>
-          </PieChart>
-        </ResponsiveContainer>
-      </>
-    );
   }
+
+  useEffect(() => {
+    setData(() => getData());
+  }, [events]);
+
+  const colors = ['#2c3e50', '#27ae60', '#f39c12', '#808080', '#3498db'];
+
+  return (
+    <>
+      <h4>Popular event genres</h4>
+      <ResponsiveContainer height={400}>
+        <PieChart width={400} height={400}>
+          <Pie
+            data={getData()}
+            cx={200}
+            cy={200}
+            labelLine={false}
+            label={({ name, percent }) => {
+              if (percent > 0) {
+                return `${name} ${(percent * 100).toFixed(0)}%`;
+              } else {
+                return null;
+              }
+            }}
+            outerRadius={80}
+            dataKey="value"
+          >
+            {getData().map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={colors[index]} />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </>
+  );
 }
 
 export { EventGenre };
